544ddd267e9b67f05649f3f0ae8c60fc9e7ffa5e
gingin77 <ginniehench@gmail.com>
2017-11-02 17:53:45 -0400

    Add an array of words, in place of using /usr/share/dict/words to test heroku issue

--- randomWord.js
+++ randomWord.js
@@ -1,5 +1,7 @@
-const fs = require('fs')
-const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
+// const fs = require('fs')
+// const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
+
+const words = ['endless', 'myriad', 'uncounted', 'untold', 'bags', 'gobs', 'heap', 'immeasurable', 'incalculable', 'infinite', 'innumerous', 'jillion', 'legion', 'limitless', 'loads', 'many', 'measureless', 'mess', 'mint', 'mucho', 'multitudinous', 'numberless', 'oodles', 'passel', 'peck', 'pile', 'raft', 'scads', 'slew', 'stack', 'tidy sum', 'umpteen', 'uncountable', 'wad', 'whole', 'slew', 'zillion']
 
 function randomWordSelector () {
   let randomWord = words[Math.floor(Math.random() * words.length)]
