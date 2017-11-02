67dd522be666408a4cf1d1256ea3e28fd4e4a48a
gingin77 <ginniehench@gmail.com>
2017-11-02 18:39:06 -0400

    Changed app.listen function to listen to process.env.PORT for production

--- app.js
+++ app.js
@@ -125,6 +125,10 @@ app.post('/newgame', function (req, res) {
   res.redirect('/')
 })
 
-app.listen(3000, function () {
-  console.log('Successfully started express application!')
+// app.listen(3000, function () {
+//   console.log('Successfully started express application!')
+// })
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env)
 })
